6563f3866e8e2eeaadcbbbd86a4fea4c1769b269
muxmaz <71841101+muxmaz@users.noreply.github.com>
2023-09-04 14:37:32 +0100

    Update streamlit_app.py
    Adding Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #New Section to display FruityVice API reponse
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
