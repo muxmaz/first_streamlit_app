0f28331254607c3d288e6c013d1e3a7bbb576ced
muxmaz <71841101+muxmaz@users.noreply.github.com>
2023-09-05 12:42:12 +0100

    Update streamlit_app.py
    Inserted a try and except condition and re-organised code

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,20 +27,20 @@ fruits_to_show =my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# To displace fruityvice API response
-
+#New Section to display FruityVice API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# puts the normalised json version of the response and inputs it into a dataframe.
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error ("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
+  
 #DON'T RUN ANYTHING PAST HERE WHILE WE TROUBLESHOOT
 streamlit.stop()
 
